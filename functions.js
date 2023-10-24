20520854124d8f3f2255ea7d8628b6f8248e9c2e
Grummps <leitetravis@gmail.com>
2023-10-23 22:26:03 -0400

    added homer quotes

--- functions.js
+++ functions.js
@@ -1,13 +1,11 @@
 var guesses = 0;
 var correctGuesses = 0;
 
-
 const Homer = ["If he's so smart, how come he's dead?", "Operator, give me the number for 911!", "Stupidity got us into this mess, and stupidity will get us out.",
 "I wish God were alive to see this.", "Fame was like a drug, but what was even more like a drug were the drugs.", "6", "7", "8", "9","10"];
 const quotes = ["To be or not to be, that is the question.", "God bless.", "2", "3", "4", "5", "6", "7", "8", "9","10"];
 var quoteUsed = "";
 var yes = false;
-
 
 const checkGuess = () => {
     if (guesses = 10) {
