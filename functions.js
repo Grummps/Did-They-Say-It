4cb3e1a969cc2bbdc0a8c7d1ec0bf3bd1706fa18
Grummps <leitetravis@gmail.com>
2023-10-23 22:26:22 -0400

    added homer quotes

--- functions.js
+++ functions.js
@@ -1,9 +1,19 @@
 var guesses = 0;
 var correctGuesses = 0;
 
-const Homer = ["If he's so smart, how come he's dead?", "Operator, give me the number for 911!", "Stupidity got us into this mess, and stupidity will get us out.",
-"I wish God were alive to see this.", "Fame was like a drug, but what was even more like a drug were the drugs.", "6", "7", "8", "9","10"];
-const quotes = ["To be or not to be, that is the question.", "God bless.", "2", "3", "4", "5", "6", "7", "8", "9","10"];
+const Homer = [
+    "If he's so smart, how come he's dead?", 
+    "Operator, give me the number for 911!", 
+    "Stupidity got us into this mess, and stupidity will get us out.",
+    "I wish God were alive to see this.", 
+    "Fame was like a drug, but what was even more like a drug were the drugs.", 
+    "You’ll have to speak up. I’m wearing a towel.",
+    "I was working on a flat tax proposal and I accidentally proved there’s no god.", 
+    "Marriage is like a coffin and each kid is another nail.", 
+    "I’m in no condition to drive…wait! I shouldn’t listen to myself, I’m drunk!",
+    "I’m a white male, age 18 to 49. Everyone listens to me, no matter how dumb my suggestions are."
+];
+const quotes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","10"];
 var quoteUsed = "";
 var yes = false;
 
