69565c41892cac0b42611b450e684d68e6da1046
Grummps <leitetravis@gmail.com>
2023-10-31 12:24:58 -0400

    splice

--- functions.js
+++ functions.js
@@ -2,8 +2,9 @@ var guesses = 0;
 var correctGuesses = 0;
 var quoteUsed = "";
 var yes = false;
+var temp = [];
 
-const Homer = [
+var Homer = [
     "If he's so smart, how come he's dead?", 
     "Operator, give me the number for 911!", 
     "Stupidity got us into this mess, and stupidity will get us out.",
@@ -16,7 +17,20 @@ const Homer = [
     "I’m a white male, age 18 to 49. Everyone listens to me, no matter how dumb my suggestions are."
 ];
 
-const quotes = [
+var quotes = [
+    "0", 
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5", 
+    "6", 
+    "7", 
+    "8", 
+    "9"
+];
+
+var mScott = [
     "0", 
     "1", 
     "2", 
@@ -41,20 +55,29 @@ const loadQuote = () => {
     let quotesIndex = Math.floor(Math.random() * quotes.length);
     const quoteId = document.getElementById("quoteId");
 
-    if (x % 2 == 0) {                               // load a Homer quote
-        let quote = document.createElement("h2");
-        quote.innerHTML = `Did Homer Simpson say: ${Homer[homerIndex]}`; // could replace "Homer Simpson" with ${nameOfCharacterChosen}
-        quoteId.append(quote);
-        // maybe quoteUsed = Homer[homerIndex]
-        // split() so no duplicates?
-    }
-    else {                                          // load from quotes
-        let quote = document.createElement("h2");
-        quote.innerHTML = `Did Homer Simpson say: ${quotes[quotesIndex]}`;
-        quoteId.append(quote);  
-        // quoteUsed = quotes[quotesIndex]
-        // split() 
-    } 
+        if (x % 2 == 0) {                               // load a Homer quote
+            let quote = document.createElement("h2");
+            quote.innerHTML = `Did Homer Simpson say: ${Homer[homerIndex]}`; // could replace "Homer Simpson" with ${nameOfCharacterChosen}
+            quoteId.append(quote);
+            
+            quoteUsed = Homer[homerIndex];
+
+            console.log(Homer[homerIndex]);
+            console.log(temp);
+            Homer.splice(homerIndex, 1);         // no duplicates
+            console.log(Homer);
+        }
+        else {                                          // load from quotes
+            let quote = document.createElement("h2");
+            quote.innerHTML = `Did Homer Simpson say: ${quotes[quotesIndex]}`;
+            quoteId.append(quote);  
+            
+            quoteUsed = quotes[quotesIndex]
+            
+            quotes.splice(quotesIndex, 1);
+        }
+        return quoteUsed;
+            
 }
 
 const checkQuote = () => { // makes "yes" the correct answer
@@ -65,5 +88,6 @@ const checkQuote = () => { // makes "yes" the correct answer
     });
     return yes;
 }
+
 
 loadQuote();
